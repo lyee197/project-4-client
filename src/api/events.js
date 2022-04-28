943d716dc5f7268275043f5990614c8db937a308
lyee197 <liamsyee@gmail.com>
2022-04-28 16:00:35 -0400

    Created Show function for events

--- src/api/events.js
+++ src/api/events.js
@@ -4,3 +4,9 @@ import axios from "axios";
 export const getAllEvents = () => {
     return axios(`${apiUrl}/events`)
 }
+
+// show function
+export const getOneEvent = (eventId) => {
+    return axios(`${apiUrl}/products/${eventId}`)
+}
+
