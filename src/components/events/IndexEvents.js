bc4fd81aff6e303eedc25449b564e9549868c49f
lyee197 <liamsyee@gmail.com>
2022-04-28 20:18:26 -0400

    Inserted useEffect in indexEvents

--- src/components/events/IndexEvents.js
+++ src/components/events/IndexEvents.js
@@ -3,8 +3,29 @@ import { getAllEvents } from '../../api/events'
 import { Link } from 'react-router-dom'
 
 const IndexEvents = (props) => {
+    const [events, setEvents ] = useState(null)
+    const { user, msgAlert } = props
 
-    
+    useEffect(() => {
+        getAllEvents()
+            .then(res => {
+                console.log('this is the res', res)
+                setEvents(res.data.events)
+            })
+            .then(() => {
+                msgAlert({
+                    heading: "Found Events",
+                    variant: 'success'
+                })
+            })
+            .catch(() => {
+                msgAlert({
+                    headking: "No event?!",
+                    varian: 'danger'
+                })
+            })
+    },[])
+
     return (
         <>
             <h1>Index Page for Events</h1>
