6f37e012bc3870cb60b435b5569f5b4e0a66559a
lyee197 <liamsyee@gmail.com>
2022-04-30 19:23:53 -0400

    Added HandleChange for CreatePet

--- src/components/pets/CreatePet.js
+++ src/components/pets/CreatePet.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import PetForm from "../shared/PetForm"
 
 const CreatePet = (props) => {
     const { user, msgAlert } = props
@@ -7,11 +8,34 @@ const CreatePet = (props) => {
     const navigate = useNavigate()
     const [pet, setPet] = useState({name: '', birthday: '', animalType: ''})
     console.log('Pet in create', pet)
+    //  an empty event object
+    // and a createdId (used to navigate)
+    // we'll need handleChange and handleSubmit funcs
+    const handleChange = (e) => {
+        e.persist()
+
+        setPet(prevPet => {
+            const name = e.target.name
+            let value = e.target.value
+            console.log('etarget type', e.target.type)
+            if (e.target.type === 'number') {
+                value = parseInt(e.target.value)
+            }
+
+            const updatedValue = { [name]: value }
+
+            console.log('prevPet', prevPet)
+            console.log('updatedValue', updatedValue)
+
+            return {...prevPet, ...updatedValue}
+        })
+    }
 
     return (
-        <>
-            <h1>Where We create Our pet!</h1>
-        </>
+        <PetForm
+        pet={pet}
+        handleChange={handleChange}
+        />
     )
 }
 
