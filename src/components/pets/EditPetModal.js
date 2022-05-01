34d9ffab938affc08123c0de525491c18bab68db
lyee197 <liamsyee@gmail.com>
2022-04-30 23:53:55 -0400

    Created Modal with PetForm

--- src/components/pets/EditPetModal.js
+++ src/components/pets/EditPetModal.js
@@ -1,13 +1,39 @@
+import { Modal } from "react-bootstrap"
 import React, { useState } from "react"
+import PetForm from "../shared/PetForm"
 
 const EditPetModal = (props) => {
     const {user, show, handleClose, updatePet, msgAlert, triggerRefresh } = props
-    const [event, setEvent] = useState(props.pet)
+    const [pet, setPet] = useState(props.pet)
+
+    const handleChange = (e) => {
+        // e === event
+        e.persist()
+
+        setPet(prevPet => {
+            const name = e.target.name
+            let value = e.target.value
+            console.log(e.target.type)
+            const updatedValue = { [name]: value}
+
+            console.log('prevPet', prevPet)
+            console.log('updatedValue', updatedValue)
+
+            return {...prevPet, ...updatedValue}
+        })
+    }
 
     return (
-        <>
-            <h1>edit</h1>
-        </>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+                <PetForm
+                    pet={pet}
+                    handleChange={handleChange}
+                    heading="Edit Pet!"
+                />
+            </Modal.Body>
+        </Modal>
     )
 }
 
