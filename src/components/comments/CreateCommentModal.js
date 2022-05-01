e32293f8a21f9a21d41745714cbe1ece4633a7f0
lyee197 <liamsyee@gmail.com>
2022-05-01 14:30:31 -0400

    Added handleChange to CreateCommentModal

--- src/components/comments/CreateCommentModal.js
+++ src/components/comments/CreateCommentModal.js
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import CommentForm from "../shared/CommentForm"
 
 const CreateCommentModal = (props) => {
+    const {user, event, show, handleClose, msgAlert, triggerRefresh } = props
+    const [comment, setComment] = useState({})
+
+    const handleChange = (e) => {
+        // e === event
+        e.persist()
+
+        setComment(prevComment => {
+            const name = e.target.name
+            let value = e.target.value
+            console.log('etarget type', e.target.type)
+            if (e.target.type === 'number') {
+                value = parseInt(e.target.value)
+            }
+            const updatedValue = { [name]: value }
+
+            console.log('prevComment', prevComment)
+            console.log('updatedValue', updatedValue)
+
+            return {...prevComment, ...updatedValue}
+        })
+    }
+
     return (
-        <h1>This is CreateCommentModal</h1>
+        <CommentForm
+            comment={comment}
+            handleChange={handleChange}
+            heading="Comment on the event!"
+        />
     )
 }
 
