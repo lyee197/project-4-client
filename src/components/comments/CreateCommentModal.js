e12fdc21188b646330bc20cb45a8f901463e1b8d
lyee197 <liamsyee@gmail.com>
2022-05-01 14:37:26 -0400

    Added handleSubmit to CreateComment

--- src/components/comments/CreateCommentModal.js
+++ src/components/comments/CreateCommentModal.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Navigate } from "react-router-dom"
 import CommentForm from "../shared/CommentForm"
 
 const CreateCommentModal = (props) => {
@@ -25,10 +26,36 @@ const CreateCommentModal = (props) => {
         })
     }
 
+    const handleSubmit = (e) => {
+        // e === event
+        e.preventDefault()
+
+        console.log('the comment to submit', comment)
+        giveComment(user, event._id, comment)
+            // if create is successful, we should navigate to the show page
+            .then(res => {Navigate(`events/${res.data.event._id}`)})
+            // then we send a success message
+            .then(() =>
+                msgAlert({
+                    heading: 'Toy given to pet!',
+                    message: 'great! the pet loves it!',
+                    variant: 'success',
+                }))
+            .then(() => triggerRefresh())
+            // if there is an error, we'll send an error message
+            .catch(() =>
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'that aint it',
+                    variant: 'danger',
+                }))
+}
+
     return (
         <CommentForm
             comment={comment}
             handleChange={handleChange}
+            handleSubmit={handleSubmit}
             heading="Comment on the event!"
         />
     )
